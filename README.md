a7f5b9b9b3e4bcd30550a89d41cf5b370d7f2a6c
Colin Fergus <chincheela@tidepoolsoftware.com>
2012-10-12 21:34:12 -0400

    Basic alpha version
    Presents a sankey visualization describing tuples that are acked between
    components in a storm topology. Intended as a first release that the
    community can comment on.

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-This is a start to a project to visualize a running storm topology
+This is a project to visualize a running storm topology
+
+## What does it do
+
+It shows a visual representation of the volume of tuples sent from one storm component to the next
+
+I stole most everything that makes this interesting. Credit is due to Nathan Marz and Michael Bostock in this regard.
+
+If this works, an image of what it presents is below:
+
+[[sankey-storm.png]]
+
+## How does it suck?
+
+Many ways!
+
+This is my first project on:
+* github
+* clojure
+* storm
+* sankey
+
+As such, there will be demons all about.
+
+Of most significance:
+* Only works on the 'default' stream
+* Pinned to port 8080 instead of the UI port
+* Probably won't work if you check it out and try to run it
+* Requires you to hand-enter the topology id into the URL
+
+Many of these deficiencies will be addressed depending on how it might be incorporated relevant to the native Storm UI
+
+## How can it be made better?
+I am interested in receiving feedback on:
+* What clojure forms/syntax would be better to use. (there are many opportunities for feedback)
+* In what ways would the visualization be more useful. No guarantees that they will be implemented.
+* In what ways could this be a better github neighbor. Such as:
+** License inclusion
+** Should it instead be a fork of storm, with a pull request
+** Have I caused any great offenses in my project structure or otherwise
--- src/main/clojure/backtype/storm/ui/sankey/sankeyview.clj
+++ src/main/clojure/backtype/storm/ui/sankey/sankeyview.clj
@@ -1,6 +1,7 @@
-;; java -cp target\;C:\Apps\storm-0.8.1\storm-0.8.1.jar;C:\Apps\storm-0.8.1\lib\*;. clojure.main -i backtype/storm/ui/sankey/sankeyview.clj -e (backtype.storm.ui.sankey/-main)
+;; java -cp target\;C:\Apps\storm-0.8.1\storm-0.8.1.jar;C:\Apps\storm-0.8.1\lib\*;.
+;; clojure.main -i backtype/storm/ui/sankey/sankeyview.clj -e (backtype.storm.ui.sankey/-main)
 
-;; todo : if no stats are present, no viz appears at all
+;; todo : if no stats are present, no viz appears at all. Instead, present a helpful error
 
 (ns backtype.storm.ui.sankey
   (:use compojure.core)
@@ -21,6 +22,7 @@
   (:gen-class))
 
 (defn sankey-ui-template [body]
+  "Include all relevant css and js relevant to creating a sankey visualization of a storm topology."
   (html
    [:head
     [:title "Storm Sankey UI"]
@@ -41,6 +43,7 @@
 ; TODO : Post filter on streamId based on desired stream to view
 ; Filter out those counts not part of the stream of interest
 (defn bolt-input-stats [window ^TopologyInfo topology-info component executors include-sys?]
+  "Extract the statistics for a topology in order to create visualizations."
   (let [stats (storm-ui/get-filled-stats executors)
         stream-summary (-> stats (storm-ui/aggregate-bolt-stats include-sys?))
         all-inputs-to-bolt (get (:acked stream-summary) window  )
@@ -55,33 +58,67 @@
 
 
 (defn generate-nodes-json [components-in-order]
+   "Generate the json required to visualize nodes/components/rectangles."
    (interpose ","
-   (map
-        #( str "{\"name\":\"" (.trim %1) "\"}" )
+     (map
+        #( str "{\"name\":\"" (trim %1) "\"}" )
         components-in-order )
    )
   )
 
 
 (defn lookup-component-index [ bolt-id components-in-order ]
-  (.indexOf components-in-order ( .trim  bolt-id ) )
+  "Given an ordered vector of component-ids, lookup the index of a component for visualization purposes."
+  (.indexOf components-in-order ( trim  bolt-id ) )
 )
 
+
+(defn generate-links-json-for-target-from-value [components-in-order vector-of-links]
+  "Given the vector of link data, generates the actual json required to
+  represent the metrics for a sankey visualization."
+  ; input is vector of maps, where each map is :target, :source, :value
+
+    (apply str
+      (interpose ","
+        (map
+          #(str "{\"source\":" (lookup-component-index (:source %) components-in-order ) ","
+             "\"target\":" ( lookup-component-index (:target %) components-in-order ) ","
+             "\"value\":" (:value %)
+             "}\n"
+             )
+          vector-of-links
+          )
+        )
+      )
+  )
+
+(defn generate-links-json-for-target [components-in-order link-stats-for-target]
+  "Given a specific target component, create json for incoming data metrics."
+
+  (apply str
+    (interpose ","
+
+     (map
+       (partial generate-links-json-for-target-from-value components-in-order)
+       (vals link-stats-for-target))
+      )
+    )
+  )
+
 (defn generate-links-json [components-in-order link-stats]
+  "Generate json to support the data required for links between nodes.
+  This represents tuples sent between a component and a bolt"
   (interpose ","
     (map
-        #( str
-           "{\"source\":" ( lookup-component-index (first (first % )) components-in-order) ","
-           "\"target\":" ( lookup-component-index (second (first %)) components-in-order ) ","
-           "\"value\":" ( nth (first %) 2 )
-           "}\n"
-         )
-       link-stats)
+      ( partial generate-links-json-for-target components-in-order)
+      link-stats
+      )
     )
 )
 
 
 (defn link-stats-for-bolt-target [bolt-id summ topology window include-sys?]
+  "Given a bolt-id, this function generates the statistics about the components that feed it."
   (let [task-summs (storm-ui/component-task-summs summ topology bolt-id)
         input-stats (bolt-input-stats window summ bolt-id task-summs include-sys?)
         count-map ( into []
@@ -89,28 +126,33 @@
                          [(.get_componentId gsid) count]) )
         ]
 
-    (map
-        #( vector
-           (.trim (first % )) (.trim bolt-id) ( second % ) )
+    ; for a given bolt, count map is [[ src count] ]
+    ; eg [[second-sentence-spout 1140] [first-sentence-spout 2560]]
+
+     (group-by :target (map
+        #( hash-map
+            :source (trim (first % )) :target (trim bolt-id) :value ( second % ) )
         count-map )
-    )
+     )
+  )
 )
 
-(defn generate-json-sankey-data [ bolts-in-order components-in-order bolt-comp-summs summ topology window include-sys? ]
-
+(defn generate-json-sankey-data [ bolts-in-order components-in-order
+                                 bolt-comp-summs summ topology window include-sys? ]
+  "Generate the javascript variables required for sankey visualization"
   (def link-stats
-        (map
-           #(link-stats-for-bolt-target % summ topology window include-sys? )
-           bolts-in-order
-       )
+    (map
+       #(link-stats-for-bolt-target % summ topology window include-sys? )
+       bolts-in-order
+    )
   )
 
   (str
-    "var tupleStats=\n"
-    "{\"nodes\":["
+    "var tupleStats=\n"  ; root level variable, used for sankey diagram
+    "{\"nodes\":["      ; the 'rectangles'
     (apply str (generate-nodes-json components-in-order) )
     "],"
-    "\"links\":["
+    "\"links\":["       ; the paths connecting rectangles/nodes
     (apply str (generate-links-json components-in-order
         (filter #(seq %) link-stats) ) )
     "]};"
@@ -118,30 +160,32 @@
 )
 
 (defn topology-sankey-page [topology-id window include-sys?]
+  "Create html relevant to visualization of a storm topology"
   (storm-ui/with-nimbus nimbus
-  (let [
-        window (if window window ":all-time")
-        summ (.getTopologyInfo ^Nimbus$Client nimbus topology-id)
-        topology (.getTopology ^Nimbus$Client nimbus topology-id)
-        bolt-summs (filter (partial storm-ui/bolt-summary? topology) (.get_executors summ))
-        spout-summs (filter (partial storm-ui/spout-summary? topology) (.get_executors summ))
-        bolt-comp-summs (storm-ui/group-by-comp bolt-summs)
-        spout-comp-summs (storm-ui/group-by-comp spout-summs)
-        bolts-in-order (into [] (keys bolt-comp-summs))
-        spouts-in-order (into [] (keys spout-comp-summs))
-        components-in-order ( concat spouts-in-order bolts-in-order )
-        ]
-    (concat
+    (let [
+          window (if window window ":all-time")
+          summ (.getTopologyInfo ^Nimbus$Client nimbus topology-id)
+          topology (.getTopology ^Nimbus$Client nimbus topology-id)
+          bolt-summs (filter (partial storm-ui/bolt-summary? topology) (.get_executors summ))
+          spout-summs (filter (partial storm-ui/spout-summary? topology) (.get_executors summ))
+          bolt-comp-summs (storm-ui/group-by-comp bolt-summs)
+          spout-comp-summs (storm-ui/group-by-comp spout-summs)
+          bolts-in-order (into [] (keys bolt-comp-summs))
+          spouts-in-order (into [] (keys spout-comp-summs))
+          components-in-order ( concat spouts-in-order bolts-in-order )
+          ]
+      (concat
 
-       [[:script (apply str
-          ( generate-json-sankey-data bolts-in-order components-in-order bolt-comp-summs summ topology window include-sys? ))
-       ]]
+         [[:script (apply str
+            ( generate-json-sankey-data bolts-in-order components-in-order
+                    bolt-comp-summs summ topology window include-sys? ))
+         ]]
 
-       [[:h2 "Sankey Chart"]
-         [:p {:id "chart"}]
-         ]
-     )
-  ))
+         [[:h2 "Sankey Chart"]
+           [:p {:id "chart"}]
+           ]
+       )
+    ))
 )
 
 
